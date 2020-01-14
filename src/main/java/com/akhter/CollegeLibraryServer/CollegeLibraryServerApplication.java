4e9f55ad83f98d81b874f3f2dda588a193fef93c
AkhterRasool <akhterrasool48@gmail.com>
2020-01-14 15:27:08 +0530

    Modify necessary files to deploy war

--- src/main/java/com/akhter/CollegeLibraryServer/CollegeLibraryServerApplication.java
+++ src/main/java/com/akhter/CollegeLibraryServer/CollegeLibraryServerApplication.java
@@ -2,9 +2,10 @@ package com.akhter.CollegeLibraryServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CollegeLibraryServerApplication {
+public class CollegeLibraryServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollegeLibraryServerApplication.class, args);
